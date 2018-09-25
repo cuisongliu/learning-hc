12750bb874073d40f597145cdb043b08ffa8742d
cuisongliu <cuisongliu@qq.com>
2018-09-25 14:47:59 +0800

    提交多线程的相关代码

---
+++ src/main/java/com/cuisongliu/concurrency/example/atomicity/atomic/AtomicExample3.java
@@ -0,0 +1,49 @@
+package com.cuisongliu.concurrency.example.atomicity.atomic;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 cuisongliu@qq.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import com.cuisongliu.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * Atomic 实现线程安全（CAS compare and swap ）
+ * @author cuisongliu [cuisongliu@qq.com]
+ * @since 2018-09-25 上午11:42
+ */
+@Slf4j
+@ThreadSafe
+public class AtomicExample3 {
+    private static AtomicReference<Integer> count = new AtomicReference<>(0);
+
+    public static void main(String[] args) {
+        count.compareAndSet(0,2);
+        count.compareAndSet(0,1);
+        count.compareAndSet(1,3);
+        count.compareAndSet(2,4);
+        count.compareAndSet(3,5);
+        log.info("atomicity:{}",count.get());
+    }
+}
---
+++ src/main/java/com/cuisongliu/concurrency/example/atomicity/sync/SynchronizedExample1.java
@@ -0,0 +1,55 @@
+package com.cuisongliu.concurrency.example.atomicity.sync;/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 cuisongliu@qq.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import com.cuisongliu.concurrency.annoations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 关键字使用
+ *
+ * @author cuisongliu [cuisongliu@qq.com]
+ * @since 2018-09-25 下午2:21
+ */
+@Slf4j
+@ThreadSafe
+public class SynchronizedExample1 {
+
+    private int count = 0;
+
+    public void test1() {
+        synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                log.info("test1 - {}", i);
+            }
+        }
+    }
+
+    public synchronized void test2() {
+        for (int i = 0; i < 10; i++) {
+            log.info("test2 - {}", i);
+        }
+    }
+
+
+}
