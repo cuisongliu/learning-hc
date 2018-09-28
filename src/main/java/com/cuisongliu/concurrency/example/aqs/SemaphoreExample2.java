1c6cce60d20555e292bc3df28e05c6d5e109e458
cuisongliu <cuisongliu@qq.com>
2018-09-28 21:36:08 +0800

    提交信号量

---
+++ src/main/java/com/cuisongliu/concurrency/example/aqs/SemaphoreExample2.java
@@ -0,0 +1,68 @@
+package com.cuisongliu.concurrency.example.aqs;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 cuisongliu@qq.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * 信号量
+ *
+ * @author cuisongliu [cuisongliu@qq.com]
+ * @since 2018-09-28 21:15
+ */
+@Slf4j
+public class SemaphoreExample2 {
+    //同时并发执行的线程数
+    private static int threadTotal = 200;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //限制每次执行10个线程
+        Semaphore semaphore = new Semaphore(10);
+        for (int i =0; i < threadTotal;i++) {
+            final int threadNum = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    test(threadNum);
+                    semaphore.release();
+                }catch (Exception e){
+                    log.info("error");
+                }
+            });
+        }
+        log.info("finish");
+        executorService.shutdown();
+    }
+
+    private static void test(int threadNum) throws InterruptedException {
+        Thread.sleep(1000);
+        log.info(threadNum+"");
+    }
+
+}
