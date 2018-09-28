dfb6136b359971538b102e67e8e45fa758608733
cuisongliu <cuisongliu@qq.com>
2018-09-28 20:43:14 +0800

    并发容器相关的类

---
+++ src/main/java/com/cuisongliu/concurrency/example/concurrent/ConcurrentHashSetExample.java
@@ -0,0 +1,57 @@
+package com.cuisongliu.concurrency.example.concurrent;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 cuisongliu@qq.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import com.cuisongliu.concurrency.annoations.ThreadSafe;
+import com.cuisongliu.concurrency.template.AbstractThreadClass;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+/**
+ * @author cuisongliu [cuisongliu@qq.com]
+ * @since 2018-09-28 20:33
+ */
+@ThreadSafe
+public class ConcurrentHashSetExample {
+    public static void main(String[] args) throws Exception {
+        new AbstractThreadClass() {
+            private Set<Integer> set = new CopyOnWriteArraySet<>();//重入锁
+            @Override
+            public void testExec() {
+
+            }
+
+            @Override
+            public void testExec(int count) {
+                set.add(count);
+            }
+
+            @Override
+            public String log4j() {
+                return set.size()+"";
+            }
+        }.main();
+    }
+}
