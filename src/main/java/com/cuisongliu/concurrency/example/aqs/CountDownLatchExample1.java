bee1a5b36c916baaa981524b55f3581d77833a41
cuisongliu <cuisongliu@qq.com>
2018-09-28 21:28:29 +0800

    提交count down latch

---
+++ src/main/java/com/cuisongliu/concurrency/example/aqs/CountDownLatchExample1.java
@@ -0,0 +1,69 @@
+package com.cuisongliu.concurrency.example.aqs;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 cuisongliu@qq.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 用法1 阻塞等待其他线程执行完毕
+ *
+ * @author cuisongliu [cuisongliu@qq.com]
+ * @since 2018-09-28 21:15
+ */
+@Slf4j
+public class CountDownLatchExample1 {
+    //同时并发执行的线程数
+    private static int threadTotal = 200;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final CountDownLatch countDownLatch = new CountDownLatch(threadTotal);
+        for (int i =0; i < threadTotal;i++) {
+            final int threadNum = i;
+            executorService.execute(() -> {
+                try {
+                    test(threadNum);
+                }catch (Exception e){
+                    log.info("error");
+                }finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        log.info("finish");
+        executorService.shutdown();
+    }
+
+    private static void test(int threadNum) throws InterruptedException {
+        Thread.sleep(100);
+        log.info(threadNum+"");
+        Thread.sleep(100);
+    }
+
+}
