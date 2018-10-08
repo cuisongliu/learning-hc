5c616ffc652375d01b08f0f246b29958d3eca527
cuisongliu <cuisongliu@qq.com>
2018-10-08 11:30:01 +0800

    cyclicBarrier 样例

---
+++ src/main/java/com/cuisongliu/concurrency/example/aqs/CyclicBarrierExample2.java
@@ -0,0 +1,67 @@
+package com.cuisongliu.concurrency.example.aqs;
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 cuisongliu@qq.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.*;
+
+/**
+ * 用法1 阻塞等待其他线程执行完毕
+ *
+ * @author cuisongliu [cuisongliu@qq.com]
+ * @since 2018-09-28 21:15
+ */
+@Slf4j
+public class CyclicBarrierExample2 {
+    //同时并发执行的线程数
+    private static int threadTotal = 200;
+    private static CyclicBarrier barrier = new CyclicBarrier(5);
+    public static void main(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i =0; i < 10;i++) {
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executorService.execute(() -> {
+                try {
+                    race(threadNum);
+                } catch (Exception e) {
+                    log.error("exception",e);                }
+            });
+        }
+        executorService.shutdown();
+    }
+
+    private static void race(int threadNum) throws Exception {
+        Thread.sleep(1000);
+        log.info(threadNum+"is ready");
+        try{
+            barrier.await(2000, TimeUnit.MILLISECONDS);
+        }catch (BrokenBarrierException | TimeoutException e ){
+            log.warn("BrokenBarrierException",e);
+        }
+        log.info(threadNum+"is continue");
+    }
+
+}
